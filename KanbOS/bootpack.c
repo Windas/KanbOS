a3e25dfa99c5f82528f195d6d3ee15f33bb2b7e8
Wind_as <Wind_as@outlook.com>
2015-04-04 23:42:32 +0800

    change color

--- KanbOS/bootpack.c
+++ KanbOS/bootpack.c
@@ -1,35 +1,32 @@
 #define COL8_000000		0
-#define COL8_FF0000 	1
-#define COL8_00FF00 	2
-#define COL8_FFFF00 	3
-#define COL8_0000FF 	4
-#define COL8_FF00FF 	5
-#define COL8_00FFFF 	6
-#define COL8_FFFFFF 	7
-#define COL8_C6C6C6 	8
-#define COL8_840000 	9
-#define COL8_008400 	10
-#define COL8_848400 	11
-#define COL8_000084 	12
-#define COL8_840084 	13
-#define COL8_008484 	14
-#define COL8_848484 	15
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
-void io_hlt(void); 					//使CPU进入待机状态
-void io_cli(void); 					//将中断标志位置0
-void io_out8(int port, int data); 
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
 
-
 void init_palette(void);
-void set_palette(int start, int end, unsigned char* rgb);
+void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-
 
 void HariMain(void)
 {
-
 	char *vram;
 	int xsize, ysize;
 
@@ -38,7 +35,7 @@ void HariMain(void)
 	xsize = 320;
 	ysize = 200;
 
-	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_000000,  0,         0,          xsize -  1, ysize - 29);
 	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
 	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
 	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
@@ -55,7 +52,7 @@ void HariMain(void)
 	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
 	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 
-	for ( ; ; ) {
+	for (;;) {
 		io_hlt();
 	}
 }
@@ -63,43 +60,41 @@ void HariMain(void)
 void init_palette(void)
 {
 	static unsigned char table_rgb[16 * 3] = {
-		0x00, 0x00, 0x00, 			//#000000黑
-		0xff, 0x00, 0x00, 			//#ff0000亮红
-		0xff, 0xff, 0x00, 			//#00ff00亮绿
-		0x00, 0x00, 0xff, 			//#0000ff亮蓝
-		0xff, 0x00, 0xff, 			//#ff00ff亮紫
-		0x00, 0xff, 0xff, 			//#00ffff浅亮蓝
-		0xff, 0xff, 0xff, 			//#ffffff白色
-		0xc6, 0xc6, 0xc6, 			//#c6c6c6亮灰
-		0x84, 0x00, 0x00, 			//#840000暗红
-		0x00, 0x84, 0x00, 			//#008400暗绿
-		0x84, 0x84, 0x00, 			//#848400暗黄
-		0x00, 0x00, 0x84, 			//#000084暗青
-		0x84, 0x00, 0x84, 			//#840084暗紫
-		0x00, 0x84, 0x84,			//#008484浅暗蓝
-		0x84, 0x84, 0x84 			//#848484暗灰
+		0x00, 0x00, 0x00,	/*  0:黑 */
+		0xff, 0x00, 0x00,	/*  1:亮红*/
+		0x00, 0xff, 0x00,	/*  2:亮绿*/
+		0xff, 0xff, 0x00,	/*  3:亮黄*/
+		0x00, 0x00, 0xff,	/*  4:亮蓝*/
+		0xff, 0x00, 0xff,	/*  5:亮紫*/
+		0x00, 0xff, 0xff,	/*  6:浅亮蓝*/
+		0xff, 0xff, 0xff,	/*  7:白*/
+		0xc6, 0xc6, 0xc6,	/*  8:亮灰*/
+		0x84, 0x00, 0x00,	/*  9:暗红*/
+		0x00, 0x84, 0x00,	/* 10:暗绿*/
+		0x84, 0x84, 0x00,	/* 11:暗黄*/
+		0x00, 0x00, 0x84,	/* 12:暗青*/
+		0x84, 0x00, 0x84,	/* 13:暗紫*/
+		0x00, 0x84, 0x84,	/* 14:浅暗蓝*/
+		0x84, 0x84, 0x84	/* 15:暗灰*/
 	};
 
-	set_palette(0, 15, table_rgb); 	//设定调色板信息
-
+	set_palette(0, 15, table_rgb);
 	return;
 }
 
-void set_palette(int start, int end, unsigned char* rgb)
+void set_palette(int start, int end, unsigned char *rgb)
 {
-	int eflags = io_load_eflags(); 		//记录中断许可标志的值
-	io_cli(); 						//将许可标志置为0，禁止中断
+	int i, eflags;
+	eflags = io_load_eflags();	/* Š„‚èž‚Ý‹–‰Âƒtƒ‰ƒO‚Ì’l‚ð‹L˜^‚·‚é */
+	io_cli(); 					/* ‹–‰Âƒtƒ‰ƒO‚ð0‚É‚µ‚ÄŠ„‚èž‚Ý‹ÖŽ~‚É‚·‚é */
 	io_out8(0x03c8, start);
-	for (int i = start; i <= end; i++) {
+	for (i = start; i <= end; i++) {
 		io_out8(0x03c9, rgb[0] / 4);
 		io_out8(0x03c9, rgb[1] / 4);
 		io_out8(0x03c9, rgb[2] / 4);
-
 		rgb += 3;
 	}
-
-	io_store_eflags(eflags); 		//恢复许可标志的值
-	
+	io_store_eflags(eflags);	/* Š„‚èž‚Ý‹–‰Âƒtƒ‰ƒO‚ðŒ³‚É–ß‚· */
 	return;
 }
 
@@ -109,4 +104,6 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 		for (int x = x0; x <= x1; x++)
 			vram[y * xsize + x] = c;
 	}
+
+	return;
 }
